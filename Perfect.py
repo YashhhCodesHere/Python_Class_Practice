0d44780efdfba270b28e89a080c1d1e688473c38
Yash Kumar <Yashxhere@gmail.com>
2024-09-14 12:17:12 +0530

    feat: Add code to check if a number is a perfect number in Perfect.py

---
+++ Perfect.py
@@ -0,0 +1,14 @@
+def is_perfect_number(n):
+    if n < 1:
+        return "Your entered number is not a perfect number"
+    else:
+        divisior = sum(i for i in range(1, n) if n%i==0)
+    if(divisior == n):
+        print("Your entered number is a perfect number!")
+    else:
+        print("Your entered number is NOT a perfect number!")
+
+
+number = int(input("Enter the number: "))
+is_perfect_number(number)
+
